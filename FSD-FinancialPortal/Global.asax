cd4e7a7890e3075923d85a94b0625ccbfe36ce63
Christian Yves Dubuche <dubuche@gmail.com>
2017-08-29 12:06:16 -0400

    Add project files.

---
+++ FSD-FinancialPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FSD_FinancialPortal.MvcApplication" Language="C#" %>
---
+++ FSD-FinancialPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FSD_FinancialPortal.Startup))]
+namespace FSD_FinancialPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
