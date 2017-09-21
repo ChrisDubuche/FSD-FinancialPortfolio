8e62a310646ea6b5c5ee361c5b61e82ee744c323
Christian Yves Dubuche <dubuche@gmail.com>
2017-09-21 16:25:44 -0400

    Added Helper Methods & Home Index layout tools

---
+++ FSD-FinancialPortal/Helpers/BudgetHelper.cs
@@ -0,0 +1,32 @@
+﻿using FSD_FinancialPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FSD_FinancialPortal.Helpers
+{
+    public class BudgetHelper
+    {
+        private static ApplicationDbContext db = new ApplicationDbContext();
+
+        public static decimal BudgetItemExpense(int budgetItemId)
+        {
+            var budgetItem = db.BudgetItems.FirstOrDefault(b => b.Id == budgetItemId);
+            decimal amount = 0;
+            foreach (var transaction in budgetItem.Transactions.ToList())
+            {
+                if (transaction.ReconciledAmount == 0)
+                {
+                    amount += transaction.Amount;
+                }
+                else
+                {
+                    amount += transaction.ReconciledAmount;
+                }
+            }
+
+            return amount;
+        }
+    }
+}
